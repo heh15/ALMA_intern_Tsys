89e47c98ba65c0417fc5ad1070ab6b0e9b2eff71
heh15 <explorationdiscovery@hotmail.com>
2021-11-09 09:00:27 -0500

    Add the script to write the gain from relation dervied by 'Tsys_WVR_part_fit.casa.py'

---
+++ scripts/Tsys_WVR_partextrap_gain.casa.py
@@ -0,0 +1,402 @@
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+
+###########################################################
+# basic settings
+
+vis = 'uid___A002_Xda1250_X2387.ms'
+filename_match = 'Tsys_WVR_matched_avgTime10.pkl'
+filename_fit = 'Tsys_WVR_part_fitted_WVRchan0_normScans000.pkl' 
+
+## information from the measurement set 
+# number of antennas 
+msmd.open(vis)
+nants = msmd.nantennas()
+msmd.done()
+iants = np.arange(0, nants, 1)
+# iants = np.delete(iants, np.argwhere(iants == 30)) # antenna 30 has weird Tsys values
+
+# scan id 
+msmd.open(vis)
+scan_ATM = msmd.scansforintent('*CALIBRATE_ATMOSPHERE*')
+scan_phase = msmd.scansforintent('*CALIBRATE_PHASE*')
+scan_checksource = msmd.scansforintent('*OBSERVE_CHECK_SOURCE*')
+scan_bpass = msmd.scansforintent('*CALIBRATE_BANDPASS*')
+scan_sci = msmd.scansforintent('*OBSERVE_TARGET*')
+msmd.done()
+
+## informatoin from the 'filename_match'.
+with open(filename_match, 'rb') as pickle_file:
+    Tsys_table = pickle.load(pickle_file)
+timebin = Tsys_table['info']['avg time']
+Tsys_spws = Tsys_table['info']['Tsys spw']
+
+obs_types = Tsys_table['obs_type'].reshape(-1,nants)[:,0]
+time_Tsys = Tsys_table['time_Tsys'].reshape(-1,nants)[:,0]
+Tsys = Tsys_table['Tsys']
+
+## information from the 'fiename_fit'
+with open(filename_fit, 'rb') as pickle_file:
+    fit_results = pickle.load(pickle_file)
+chan_WVR = fit_results['info']['WVR chan']
+normScans = fit_results['info']['norm scans']
+ATM_No = fit_results['info']['No. ATM']
+
+#############################
+# WVR gain table information
+
+# column names of Tsys table dictionary
+WVR_table = {}
+
+# info columns
+WVR_table['info'] = {}
+info_columns = ['WVR chan','Tsys spw','avg time']
+WVR_table['info'] = dict.fromkeys(info_columns)
+WVR_table['info']['WVR chan'] = chan_WVR
+WVR_table['info']['Tsys spw'] = Tsys_spws
+WVR_table['info']['avg time'] = timebin
+WVR_table['info']['norm scans'] = normScans
+WVR_table['info']['No. ATM'] = ATM_No 
+
+# filename of the output pickle file
+filename_WVR = 'WVR_partextrap_gaintable_chanWVR'+str(chan_WVR)+'_avgtime'+str(timebin)+'.pkl'
+
+###########################################################
+# functions
+def array_isin(element, test_elements):
+    '''
+    test if element in one array is in another array
+    ------
+    parameters
+    element: np.ndarray
+        input array
+    test_elements: np.ndarray
+        the values against which to test each value of element. this 
+        argument is flattened if it is an array or array_like. 
+    ------
+    return
+    isin: np.ndarray
+        indexes of element that in test_elements. 
+    '''
+    dist = np.abs(element[:, np.newaxis] - test_elements)
+    diff = np.nanmin(dist, axis=1)
+    isin = np.where(diff == 0)
+
+    return isin
+
+def find_nearest(array, values, full=False):
+    '''
+    find the index of the values in the array closest to the given value
+    ------
+    parameters:
+    array: numpy.ndarray
+        numpy array to be matched
+    values: numpy.ndarray or float
+        values to be matched with numpy array
+    full: bool, optional
+        switch determining the nature of return values. when it is false
+        (default) just the index returned. when true, the difference 
+        between matched array and given values are also returned
+    ------
+    return:
+    idx: int or int numpy array
+        index of the closest value in array.
+    diff: float or float numpy array
+        present only if `full`=true. absolute difference between the 
+        given values and matched values in the array. 
+    '''
+    array = np.asarray(array)
+    dist = np.abs(array[:,np.newaxis] - values)
+    idx = np.nanargmin(dist, axis=0)
+    diff = np.nanmin(dist, axis=0)
+
+    if full:
+        return idx, diff
+    else:
+        return idx
+
+
+def filter_data(data, time, lowertimes, uppertimes):
+    '''
+    filter out data within a list of certain time ranges
+    ------
+    parameters:
+    data: np.1darray
+        array of data to be filtered
+    time: np.1darray
+        time series of the data taken
+    lowertimes: np.1darray
+        array of lower limit of time ranges
+    uppertimes: np.ndarray
+        arrray of upper limit of time ranges
+    ------
+    return:
+    data_matched: np.1darray
+        data that was filtered out
+    idxsecs: np.1darray
+        list of indices for different time ranges
+    '''
+    idxsecs = []
+    for i, lowertime in enumerate(lowertimes):
+        conditions = ((time >= lowertimes[i]) & (time <= (uppertimes[i])))
+        idxsec = np.where(conditions)
+        idxsecs.append(idxsec[0])
+    idxsecs = np.array(idxsecs)
+
+    data_matched = []
+    for idxsec in idxsecs:
+        data_temp = data[idxsec]
+        data_matched.append(data_temp)
+
+    return data_matched, idxsecs
+
+def count_time(stop, start):
+    '''
+    convert the time difference into human readable form. 
+    '''
+    dure=stop-start
+    m,s=divmod(dure,60)
+    h,m=divmod(m,60)
+    print("%d:%02d:%02d" %(h, m, s))
+
+    return
+
+def rebin_WVR(WVR, time, WVR_scans, timebin=10):
+
+    isnan_idx = np.isnan(time)
+    WVR = WVR[~isnan_idx] 
+    time = time[~isnan_idx]
+
+    WVR_groups = np.split(WVR, np.unique(WVR_scans, return_index=True)[1])
+    time_groups = np.split(time, np.unique(WVR_scans, return_index=True)[1])
+    WVR_groups_new = []
+    time_groups_new = []
+
+    for i, WVR_scan in enumerate(np.unique(WVR_scans)):
+        
+        if len(time_groups[i]) == 0:
+            continue
+
+        data = WVR_groups[i]
+        start_time = time_groups[i][0]
+        stop_time = time_groups[i][-1]
+        time_binned = np.arange(start_time, stop_time, timebin) + timebin/2
+        data_binned = np.full(np.shape(time_binned)[0:1]+np.shape(WVR)[1:],
+                np.nan)
+
+        # match the WVR in the each group with the new regrided time
+        dist = np.abs(time_groups[i][:, np.newaxis] - time_binned)
+        potentialclosest = dist.argmin(axis=1)
+        diff = dist.min(axis=1)
+        # leave out the time with spacing greater than the interval of original time.
+        data[np.where(diff > timebin)] = np.nan
+        closestfound, closestcounts = np.unique(potentialclosest, return_counts=True)
+        data_group = np.split(data, np.cumsum(closestcounts)[:-1])
+        for j, index in enumerate(closestfound):
+            data_binned[index] = np.nanmean(data_group[j],axis=0)
+        WVR_groups_new.append(data_binned)
+        time_groups_new.append(time_binned)
+
+    WVR_out = np.concatenate(WVR_groups_new)
+    time_out = np.concatenate(time_groups_new)
+
+    return WVR_out, time_out
+
+
+def normalize_data(data, iants, obs_types, normScans=[0,0,0]):
+    '''
+    Normalize the data by the first measurement of each antenna
+    and each type of target (bandpass, phase and science)
+    ---
+    Parameters:
+    data: np.1darray
+        The data to be normalized. 
+    iants:np.1darray
+        Array of the antennas corresponding to the data
+    obs_type: np.1darray
+        Arrray of observation types ('bandpass', 'phase' and 
+        'science'. 
+    normScans: list, optional
+        Specify which measurement (1st, 2nd, ...) for each 
+        type of observation of each antenna to be normalized to.
+    ---
+    Return:
+    data_norm: np.1darray
+        The normalized data. 
+    '''
+
+    data_norm = np.full(np.shape(data), fill_value=np.nan)
+    iants_uq = np.unique(iants)
+    obsTypes_uq = np.unique(obs_types)
+    for iant in iants_uq:
+        for i, obs in enumerate(obsTypes_uq):
+            conditions = ((iants == iant) & (obs_types==obs))
+            indices = np.where(conditions)
+            data_sub = data[indices]
+            data_norm[indices] = data_sub / data_sub[normScans[i]]
+
+    return data_norm
+
+
+###########################################################
+# main program
+start = time.time()
+
+### read the WVR data 
+## read the data
+tb.open(vis)
+spw_WVR = 4
+ddid = spw_WVR
+scan_exclude = list(scan_checksource)
+dat = tb.query('ANTENNA1==ANTENNA2 && DATA_DESC_ID==%d && SCAN_NUMBER not in %s'%(ddid,scan_exclude))
+WVR = np.real(dat.getcol('DATA'))
+WVR_time = dat.getcol('TIME').reshape(-1, nants)[:,0]
+WVR_scans = dat.getcol('SCAN_NUMBER').reshape(-1, nants)[:,0]
+WVR_field = dat.getcol('FIELD_ID').reshape(-1, nants)[:,0]
+WVR_flag = dat.getcol('FLAG').reshape(-1, nants)[:,0]
+tb.close()
+
+WVR_temp = WVR[0].reshape(4, -1, nants)
+WVR_temp = np.swapaxes(WVR_temp, 0, 1)
+
+## exclude the hot and cold load in the WVR measurement
+indices_load = np.unique(np.where(WVR_temp[:,3,:]>150)[0])
+WVR_time[indices_load] = np.nan
+WVR_temp[indices_load,:,:] = np.nan
+
+## select WVR from the single channel
+WVR_sinchan = WVR_temp[:,chan_WVR,:]
+
+## rebin the WVR for every 10 seconds.
+
+WVR_sinchan_binned, WVR_time_binned = rebin_WVR(WVR_sinchan,WVR_time,WVR_scans,timebin=timebin)
+WVR_scans_temp = WVR_scans.astype(float) 
+WVR_scans_binned  = rebin_WVR(WVR_scans_temp,WVR_time,WVR_scans,timebin=timebin)[0]
+WVR_scans_binned[np.isnan(WVR_scans_binned)] = -1
+WVR_scans_binned = WVR_scans_binned.astype(int)
+
+WVR_field_temp = WVR_field.astype(float)
+WVR_field_binned  = rebin_WVR(WVR_field_temp,WVR_time,WVR_scans,timebin=timebin)[0]
+WVR_field_binned[np.isnan(WVR_field_binned)] = -1
+WVR_field_binned = WVR_field_binned.astype(int)
+
+## remove the nan values in the rebinned data
+indices_nan = np.unique(np.where(np.isnan(WVR_sinchan_binned))[0])
+WVR_sinchan_binned[indices_nan,:] = np.nan 
+WVR_time_binned = WVR_time_binned[~np.isnan(WVR_sinchan_binned[:,0])]
+WVR_scans_binned = WVR_scans_binned[~np.isnan(WVR_sinchan_binned[:,0])]
+WVR_field_binned = WVR_field_binned[~np.isnan(WVR_sinchan_binned[:,0])]
+WVR_sinchan_binned = WVR_sinchan_binned[~np.isnan(WVR_sinchan_binned[:,0]),:]
+     
+## classify WVR data into different types of observation
+obs_types_WVR = np.full(np.shape(WVR_time_binned), fill_value=' ', dtype="S10")
+elements = WVR_scans_binned
+
+test_elements = np.array(list(scan_phase)+list(np.intersect1d((scan_phase-1), scan_ATM)))
+isin_phase_WVR = array_isin(elements, test_elements)
+obs_types_WVR[isin_phase_WVR] = 'phase'
+
+test_elements = np.array(list(scan_sci)+list(np.intersect1d((scan_sci-1), scan_ATM)))
+isin_sci_WVR = array_isin(elements, test_elements)
+obs_types_WVR[isin_sci_WVR] = 'science'
+
+test_elements = np.array(list(scan_bpass)+list(np.intersect1d((scan_bpass-1), scan_ATM)))
+isin_bpass_WVR = array_isin(elements, test_elements)
+obs_types_WVR[isin_bpass_WVR] = 'bandpass'
+
+### normalize the WVR and extrapolate Tsys based on the start of Tsys
+
+# normalize the WVR data
+WVR_norm_binned = np.full(np.shape(WVR_sinchan_binned), np.nan)
+obsTypes_uq = np.unique(obs_types)
+for i, obs in enumerate(obsTypes_uq):
+    isin_obs = np.where(obs_types == obs)
+    isin_obs_WVR = np.where(obs_types_WVR == obs)
+    time_Tsys_start = time_Tsys[isin_obs][normScans[i]]
+    ind0 = find_nearest(WVR_time_binned, time_Tsys_start)
+    WVR_obs = WVR_sinchan_binned[isin_obs_WVR] 
+    WVR_norm_binned[isin_obs_WVR] = WVR_obs / WVR_sinchan_binned[ind0]
+
+# calculate the normalized Tsys from normalized WVR
+Tsys_reshaped = Tsys.reshape(-1,nants,4)
+Tsys_norm_ext = np.full(np.shape(WVR_norm_binned)+(4,), np.nan)
+for i, spw in enumerate(Tsys_spws):
+    Tsys_norm_ext[:,:,i] = fit_results[spw]['coeff'][0] * WVR_norm_binned +\
+            fit_results[spw]['coeff'][1]
+
+# calculate the extrapolated Tsys.
+Tsys_ext = np.full(np.shape(WVR_norm_binned)+(4,), np.nan)
+Tsys_start = np.full(np.shape(WVR_norm_binned)+(4,), np.nan)
+Tsys_orig = np.full(np.shape(WVR_norm_binned)+(4,), np.nan)
+for i, obs in enumerate(obsTypes_uq):
+    isin_obs = np.where(obs_types == obs)[0]
+    isin_obs_WVR = np.where(obs_types_WVR == obs)[0]
+    Tsys_start_value = Tsys_reshaped[isin_obs][normScans[i]]
+    Tsys_ext[isin_obs_WVR] = np.einsum('jk,ijk->ijk', Tsys_start_value, 
+            Tsys_norm_ext[isin_obs_WVR])
+    Tsys_start[isin_obs_WVR] = Tsys_start_value
+
+    # match the original Tsys with the extrapolated Tsys
+    scan_type = scan_ATM[isin_obs] + 1
+    inds_reverse, diff = find_nearest(scan_type, WVR_scans_binned, full=True)
+    inds_matched = np.where(diff==0)
+    if len(inds_matched) > 0:
+        Tsys_orig[inds_matched] = Tsys_reshaped[isin_obs][inds_reverse[inds_matched]]
+    # match the ATM cal
+    inds_reverse, diff = find_nearest(scan_ATM[isin_obs], WVR_scans_binned, full=True)
+    inds_matched = np.where(diff==0)
+    if len(inds_matched) > 0:
+        Tsys_orig[inds_matched] = Tsys_reshaped[isin_obs][inds_reverse[inds_matched]]
+
+# remove the nan values in WVR_norms_binned
+nan_mask = np.isnan(WVR_norm_binned[:,0])
+WVR_time_binned = WVR_time_binned[~nan_mask]
+WVR_scans_binned = WVR_scans_binned[~nan_mask]
+WVR_field_binned = WVR_field_binned[~nan_mask]
+obs_types_WVR = obs_types_WVR[~nan_mask]
+WVR_sinchan_binned = WVR_sinchan_binned[~nan_mask,:]
+WVR_norm_binned = WVR_norm_binned[~nan_mask,:]
+Tsys_norm_ext = Tsys_norm_ext[~nan_mask,:,:]
+Tsys_ext = Tsys_ext[~nan_mask,:,:]
+Tsys_start = Tsys_start[~nan_mask,:,:]
+Tsys_orig = Tsys_orig[~nan_mask,:,:]
+
+# remove the axis for different antennas. 
+WVR_data_out = WVR_sinchan_binned.flatten()
+WVR_norm_out = WVR_norm_binned.flatten()
+Tsys_norm_out = Tsys_norm_ext.reshape(-1,4)
+Tsys_ext_out = Tsys_ext.reshape(-1,4)
+Tsys_start_out = Tsys_start.reshape(-1,4)
+Tsys_orig_out = Tsys_orig.reshape(-1,4)
+
+### write the WVR table into the dictionary.
+WVR_table['iant'] = np.tile(np.arange(nants), len(WVR_time_binned))
+WVR_table['scan'] = np.repeat(WVR_scans_binned, nants)
+WVR_table['field'] = np.repeat(WVR_field_binned, nants)
+WVR_table['obs_type'] = np.repeat(obs_types_WVR, nants)
+WVR_table['WVR_time'] = np.repeat(WVR_time_binned, nants)
+WVR_table['WVR_data'] = WVR_data_out
+WVR_table['WVR_norm'] = WVR_norm_out
+WVR_table['Tsys_norm'] = Tsys_norm_out
+WVR_table['Tsys_ext'] = Tsys_ext_out
+WVR_table['Tsys_start'] = Tsys_start_out
+WVR_table['Tsys_orig'] = Tsys_orig_out
+
+with open(filename_WVR, 'wb') as handle:
+    pickle.dump(WVR_table, handle)
+
+stop = time.time()
+count_time(stop, start)
+
+# test
+iants = WVR_table['iant']
+Tsys_sinant = Tsys_ext[:,10,0]
+Tsys_orig_sinant = Tsys_orig[:,10,0]
+time_sinant = WVR_table['WVR_time'][np.where(iants==10)]
+fig = plt.figure()
+plt.scatter(time_sinant, Tsys_sinant, color='blue')
+plt.scatter(time_sinant, Tsys_orig_sinant, color='red')
+plt.scatter(time_Tsys, Tsys_reshaped[:,10,0],color='red')
+
---
+++ scripts/Tsys_write_partextrap.casa.py
@@ -0,0 +1,313 @@
+import time
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+
+###########################################################
+# basic settings
+
+vis = 'uid___A002_Xda1250_X2387.ms'
+
+filename_Tsys = 'Tsys_WVR_matched_avgTime10.pkl' 
+filename_gain = 'WVR_partextrap_gaintable_chanWVR0_avgtime10.pkl' 
+
+# number of antennas 
+msmd.open(vis)
+nants = msmd.nantennas()
+msmd.done()
+iants = np.arange(0, nants, 1)
+
+# scan id 
+msmd.open(vis)
+scan_ATM = msmd.scansforintent('*CALIBRATE_ATMOSPHERE*')
+scan_phase = msmd.scansforintent('*CALIBRATE_PHASE*')
+scan_checksource = msmd.scansforintent('*OBSERVE_CHECK_SOURCE*')
+scan_bpass = msmd.scansforintent('*CALIBRATE_BANDPASS*')
+scan_sci = msmd.scansforintent('*OBSERVE_TARGET*')
+msmd.done()
+
+###########################################################
+# functions
+def array_isin(element, test_elements):
+    '''
+    Test if element in one array is in another array
+    ------
+    Parameters
+    element: np.ndarray
+        Input array
+    test_elements: np.ndarray
+        The values against which to test each value of element. This 
+        argument is flattened if it is an array or array_like. 
+    ------
+    Return
+    isin: np.ndarray
+        Indexes of element that in test_elements. 
+    '''
+    dist = np.abs(element[:, np.newaxis] - test_elements)
+    diff = np.nanmin(dist, axis=1)
+    isin = np.where(diff == 0)
+
+    return isin
+
+def average_Tsys(Tsys_spectrum, chan_trim=5, average_spw=False, spws=[]):
+    '''
+    Average the system temperature over the frequency axis
+    ------
+    Parameters
+    Tsys_spectrum: np.ndarray
+        The extracted Tsys spectrum from tb.getcol()
+    chan_trim: int
+        number of channels trimed at the edge of spectrum
+    average_spw: bool
+        This determines whether to average Tsys measurements over different 
+    spectral windows. 
+    spws: np.ndarray
+        The extracted spectral window array for Tsys measurements
+    ------
+    Return 
+    Tsys: np.ndarray
+        Averaged Tsys
+    '''
+    Tsys_avg1 = np.mean(Tsys_spectrum, axis=0)
+    Tsys_avg2 = Tsys_avg1[chan_trim: (len(Tsys_avg1)-chan_trim)]
+    Tsys = np.mean(Tsys_avg2, axis=0)
+
+    if (average_spw == True) and len(spws) != 0:
+        spw_unique = np.unique(spws)
+        shape = (len(spw_unique), len(Tsys)/len(spw_unique))
+        Tsys_temp = np.full(shape, np.nan)
+        for i, spw in enumerate(np.unique(spws)):
+            Tsys_temp[i] = Tsys[np.where(spws==spw)]
+
+        Tsys = np.mean(Tsys_temp, axis=0)
+
+    return Tsys
+
+def find_nearest(array, values):
+    '''
+    Find the index of the values in the array closest to the given value
+    ------
+    Parameters:
+    array: numpy.ndarray
+        numpy array to be matched
+    values: numpy.ndarray or float64
+        values to be matched with numpy array
+    ------
+    Return:
+    idx: int
+        index of the closest value in array.
+    '''
+    array = np.asarray(array)
+    idx = np.nanargmin((np.abs(array[:,np.newaxis] - values)), axis=0)
+
+    return idx
+
+def filter_data(data, time, lowerTimes, upperTimes):
+    '''
+    Filter out data within a list of certain time ranges
+    ------
+    parameters:
+    data: np.1darray
+        array of data to be filtered
+    time: np.1darray
+        time series of the data taken
+    lowerTimes: np.1darray
+        array of lower limit of time ranges
+    upperTimes: np.ndarray
+        arrray of upper limit of time ranges
+    ------
+    return:
+    data_matched: np.1darray
+        Data that was filtered out
+    idxSecs: np.1darray
+        List of indices for different time ranges
+    '''
+    idxSecs = []
+    for i, lowerTime in enumerate(lowerTimes):
+        conditions = ((time >= lowerTimes[i]) & (time <= (upperTimes[i])))
+        idxSec = np.where(conditions)
+        idxSecs.append(idxSec[0])
+    idxSecs = np.array(idxSecs)
+
+    data_matched = []
+    for idxSec in idxSecs:
+        data_temp = data[idxSec]
+        data_matched.append(data_temp)
+
+    return data_matched, idxSecs
+
+def count_time(stop, start):
+    '''
+    Convert the time difference into human readable form. 
+    '''
+    dure=stop-start
+    m,s=divmod(dure,60)
+    h,m=divmod(m,60)
+    print("%d:%02d:%02d" %(h, m, s))
+
+    return
+
+def rebin_time(data, time, timebin=10, method='mean'):
+    
+    time_sep = np.arange(time[0], time[-1], timebin) + timebin/2
+    data_binned = np.full(np.shape(time_sep), np.nan)
+
+    # following code are similar as that in regrid_time()
+    dist = np.abs(time[:, np.newaxis] - time_sep)
+    potentialClosest = dist.argmin(axis=1)
+    diff = dist.min(axis=1)
+    # leave out the time with spacing greater than the interval of original time.
+    data[np.where(diff > timebin)] = np.nan         
+    closestFound, closestCounts = np.unique(potentialClosest, return_counts=True)
+    data_group = np.split(data, np.cumsum(closestCounts)[:-1])
+    if method == 'mean':
+        for i, index in enumerate(closestFound):
+            data_binned[index] = np.nanmean(data_group[i])
+    if method == 'counts':
+        for i, index in enumerate(closestFound):
+            value, counts = np.unique(data_group[i], return_counts=True)
+            ind = np.nanargmax(counts)
+            data_binned[index] = value[ind]   
+
+    return data_binned, time_sep
+
+
+###########################################################
+# main program
+start = time.time()
+
+### create the alternative Tsys tables
+vis_tsys_in = vis + '.tsys'
+vis_tsys_out = vis.replace('.ms', '_partextrap.ms.tsys')
+rmtables(vis_tsys_out)
+os.system('cp -r '+vis_tsys_in+' '+vis_tsys_out)
+
+tb.open(vis_tsys_out, nomodify=False)
+nrows = tb.nrows()
+tb.removerows(np.array(range(nrows)))
+tb.flush()
+tb.close()
+
+### create a new gain table
+# copy the info from amplitude gain table
+vis_amp_gain = vis+'.split.ampli_inf'
+vis_tsys_gain = vis.replace('.ms', '_partextrap_tsys_WVR.gcal') 
+rmtables(vis_tsys_gain)
+os.system('cp -r '+vis_amp_gain+' '+vis_tsys_gain)
+
+tb.open(vis_tsys_gain, nomodify=False)
+nrows = tb.nrows()
+tb.removerows(np.array(range(nrows)))
+tb.putkeyword("MSName", vis) 
+tb.flush()
+tb.close()
+
+### read the information of the Tsys table
+with open(filename_Tsys,'rb') as pickle_file:
+    Tsys_table = pickle.load(pickle_file)
+spws = np.array(Tsys_table['info']['Tsys spw'])
+iants = Tsys_table['iant']
+obs_types = Tsys_table['obs_type'][np.where(iants==0)]
+obsTypes_uq = np.unique(obs_types)
+
+### read the extrapolated normalized Tsys
+with open(filename_gain, 'rb') as pickle_file:
+    WVR_table = pickle.load(pickle_file)
+WVR_time_binned = WVR_table['WVR_time']
+gain_time = np.tile(WVR_table['WVR_time'],4)
+gain_fields = np.tile(WVR_table['field'],4)
+gain_iants = np.tile(WVR_table['iant'],4)
+gain_scans = np.tile(WVR_table['scan'],4)
+gain_data = np.transpose(WVR_table['Tsys_norm']).flatten()
+gain_data = np.sqrt(1/gain_data)
+gain_data = gain_data[np.newaxis,np.newaxis,:]
+
+### write the normalized WVR data into gain table
+tb.open(vis_tsys_gain, nomodify=False)
+tb.addrows(len(gain_time))
+tb.putcol('TIME', gain_time)
+tb.putcol('FIELD_ID', gain_fields)
+tb.putcol('SPECTRAL_WINDOW_ID', np.repeat(spws, len(WVR_time_binned)))
+tb.putcol('ANTENNA1', gain_iants)
+tb.putcol('ANTENNA2', np.full(np.shape(gain_time), -1))
+tb.putcol('INTERVAL', np.full(np.shape(gain_time), 0))
+tb.putcol('SCAN_NUMBER', gain_scans)
+tb.putcol('OBSERVATION_ID', np.full(np.shape(gain_time),0))
+tb.putcol('CPARAM', gain_data)
+tb.putcol('PARAMERR', np.full(np.shape(gain_data), 0))
+tb.putcol('FLAG', np.full(np.shape(gain_data), False))
+tb.putcol('SNR', np.full(np.shape(gain_data), 1))
+tb.flush()
+tb.close()
+
+
+### read the start of Tsys for each antenna and type of observation
+normScans = WVR_table['info']['norm scans'] 
+
+## read the Tsys information
+tb.open(vis_tsys_in)
+dat = tb.query('')
+Tsys_specs = dat.getcol('FPARAM')
+Tsys_time = dat.getcol('TIME')
+Tsys_fields = dat.getcol('FIELD_ID')
+Tsys_spws = dat.getcol('SPECTRAL_WINDOW_ID')
+Tsys_iants = dat.getcol('ANTENNA1')
+Tsys_scans = dat.getcol('SCAN_NUMBER')
+Tsys_flags = dat.getcol('FLAG')
+Tsys_paramerrs = dat.getcol('PARAMERR')
+Tsys_snrs = dat.getcol('SNR')
+tb.close()
+
+## read the start Tsys into the new Tsys table. 
+# get the index of Tsys to be extracted
+idx1d = np.arange(0,len(Tsys_time),1)
+idx1d_reshaped = idx1d.reshape(4,-1,nants)
+idx1d_extrc = np.full((4,len(obsTypes_uq),nants),np.nan)
+for i, obs in enumerate(obsTypes_uq):
+    isin_obs = np.where(obs_types==obs)
+    idx1d_extrc[:,i,:] = idx1d_reshaped[:,isin_obs[0][normScans[i]],:]
+idx1d_out = idx1d_extrc.reshape(-1).astype('int')
+
+# output the selected values into the new Tsys table
+tb.open(vis_tsys_out, nomodify=False)
+tb.addrows(len(idx1d_out))
+tb.putcol('TIME', Tsys_time[idx1d_out])
+tb.putcol('FIELD_ID', Tsys_fields[idx1d_out])
+tb.putcol('SPECTRAL_WINDOW_ID',Tsys_spws[idx1d_out])
+tb.putcol('ANTENNA1', Tsys_iants[idx1d_out])
+tb.putcol('ANTENNA2', np.full(np.shape(idx1d_out), -1))
+tb.putcol('INTERVAL', np.full(np.shape(idx1d_out), 0))
+tb.putcol('SCAN_NUMBER', Tsys_scans[idx1d_out])
+tb.putcol('OBSERVATION_ID', np.full(np.shape(idx1d_out), 0))
+tb.putcol('FPARAM', Tsys_specs[:,:,idx1d_out])
+tb.putcol('PARAMERR', Tsys_paramerrs[:,:,idx1d_out])
+tb.putcol('FLAG', Tsys_flags[:,:,idx1d_out])
+tb.putcol('SNR', Tsys_snrs[:,:,idx1d_out])
+tb.flush()
+tb.close()
+
+# # test1, gaintable
+# fig = plt.figure()
+# gain_time_sinant = gain_time[np.where(gain_iants==0)]
+# gain_data_sinant = gain_data[0][0][np.where(gain_iants==0)]
+# plt.scatter(gain_time_sinant, gain_data_sinant)
+# 
+# # test2, Tsys table
+# fig = plt.figure()
+# Tsys_out_avg = np.mean(Tsys_specs[:,:,idx1d_out], axis=(0,1))
+# Tsys_iants_out = Tsys_iants[idx1d_out]
+# Tsys_time_out = Tsys_time[idx1d_out]
+# Tsys_sinant = Tsys_out_avg[np.where(Tsys_iants_out==0)]
+# Tsys_time_sinant = Tsys_time_out[np.where(Tsys_iants_out==0)]
+# 
+# gain_time_sinant = gain_time[np.where(gain_iants==0)]
+# Tsys_ext_sinant = np.transpose(WVR_table['Tsys_ext']).flatten()[np.where(gain_iants==0)]
+# 
+# plt.scatter(gain_time_sinant, Tsys_ext_sinant, color='blue')
+# plt.scatter(Tsys_time_sinant, Tsys_sinant, color='red')
+
+
+stop = time.time()
+count_time(stop, start)
+
+

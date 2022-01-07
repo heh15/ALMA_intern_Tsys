e1c5e46424d9c0e07ed32e53fd6f83fdd9b323e8
heh15 <explorationdiscovery@hotmail.com>
2022-01-06 21:51:18 -0500

    Modify the scatter calculation for 'Tsys_WVR_part_fit.casa.py' and add the Tsys variation in the output pkl file

--- scripts/Tsys_WVR_fit.casa.py
+++ scripts/Tsys_WVR_fit.casa.py
@@ -8,6 +8,28 @@ To fit the linear relation between the normalized WVR data with Tsys data
 import pickle
 import numpy as np
 
+def array_isin(element, test_elements):
+    '''
+    test if element in one array is in another array
+    ------
+    parameters
+    element: np.ndarray
+        input array
+    test_elements: np.ndarray
+        the values against which to test each value of element. this 
+        argument is flattened if it is an array or array_like. 
+    ------
+    return
+    isin: np.ndarray
+        indexes of element that in test_elements. 
+    '''
+    dist = np.abs(element[:, np.newaxis] - test_elements)
+    diff = np.nanmin(dist, axis=1)
+    isin = np.where(diff == 0)
+
+    return isin
+
+
 def normalize_data(data, iants, obs_types, normScans=[0,0,0]):
     '''
     Normalize the data by the first measurement of each antenna
@@ -47,8 +69,11 @@ def normalize_data(data, iants, obs_types, normScans=[0,0,0]):
 
 filename = 'Tsys_WVR_matched_avgTime10.pkl'
 
-WVR_chan = 3
+# bad antennas 
+bad_ants = np.array([30,36])
 
+# parameter choices used for the fitting
+WVR_chan = 0
 normScans = [0,0,0]
 normScans_txt = ''.join([str(i) for i in normScans])
 
@@ -71,6 +96,10 @@ obs_types = Tsys_table['obs_type']
 WVR_means = Tsys_table['WVR_means'][:,WVR_chan]
 Tsys = Tsys_table['Tsys']
 
+# exclude the bad antennas
+if len(bad_ants) > 0:
+    Tsys[array_isin(iants, bad_ants),:] = np.nan
+
 ## normalize Tsys and WVR data.
 WVR_norms = normalize_data(WVR_means, iants, obs_types, normScans=normScans)
 
@@ -85,6 +114,7 @@ for key in fit_results.keys():
 
 for i, spw in enumerate(Tsys_spws_set):
     xdata = WVR_norms; ydata = Tsys_norms[:,i]
+    yrange = (np.nanmax(ydata)-np.nanmin(ydata)) / np.nanmean(ydata)
     idx_nnan = ((~np.isnan(xdata)) & (~np.isnan(ydata)))
     results = np.polyfit(xdata[idx_nnan], ydata[idx_nnan], 1, full=True)
     fit_coeff = results[0]
@@ -92,6 +122,7 @@ for i, spw in enumerate(Tsys_spws_set):
     fit_err_rel = np.sqrt(fit_err / len(ydata[idx_nnan])) / np.mean(ydata[idx_nnan])
     fit_results[spw]['coeff'] = fit_coeff
     fit_results[spw]['rel_err'] = fit_err_rel
+    fit_results[spw]['Tsys_range'] = yrange
 
 ## add the basic information of the fit_results
 fit_results['info'] = {}
--- scripts/Tsys_WVR_part_fit.casa.py
+++ scripts/Tsys_WVR_part_fit.casa.py
@@ -8,6 +8,27 @@ To fit the linear relation between the normalized WVR data with Tsys data
 import pickle
 import numpy as np
 
+def array_isin(element, test_elements):
+    '''
+    test if element in one array is in another array
+    ------
+    parameters
+    element: np.ndarray
+        input array
+    test_elements: np.ndarray
+        the values against which to test each value of element. this 
+        argument is flattened if it is an array or array_like. 
+    ------
+    return
+    isin: np.ndarray
+        indexes of element that in test_elements. 
+    '''
+    dist = np.abs(element[:, np.newaxis] - test_elements)
+    diff = np.nanmin(dist, axis=1)
+    isin = np.where(diff == 0)
+
+    return isin
+
 def normalize_data(data, iants, obs_types, normScans=[0,0,0]):
     '''
     Normalize the data by the first measurement of each antenna
@@ -49,7 +70,6 @@ def select_ATM(data, iants, ATM_No):
     Parameters
     data: np.1darray
         1D array of data to be selected from 
-spws_all = np.arange(0,len(tsysmap),1).astype(int)
     iants: np.1darray
         Array of antenna ids associated with data
     ATM_No: list
@@ -72,7 +92,9 @@ spws_all = np.arange(0,len(tsysmap),1).astype(int)
 # basic settings
 
 filename = 'Tsys_WVR_matched_avgTime10.pkl'
+bad_ants = np.array([30,36])
 
+# Parameter choices used for the fitting
 WVR_chan = 0
 
 normScans = [0,0,0]
@@ -100,6 +122,10 @@ obs_types = Tsys_table['obs_type']
 WVR_means = Tsys_table['WVR_means'][:,WVR_chan]
 Tsys = Tsys_table['Tsys']
 
+# exclude bad antennas
+if len(bad_ants) > 0:
+    Tsys[array_isin(iants, bad_ants),:] = np.nan
+
 ## normalize Tsys and WVR data.
 WVR_norms = normalize_data(WVR_means, iants, obs_types, normScans=normScans)
 
@@ -119,14 +145,17 @@ for key in fit_results.keys():
     fit_results[key]= dict.fromkeys(['coeff','rel_err'])
 
 for i, spw in enumerate(Tsys_spws_set):
+    Tsys_range = (np.nanmax(Tsys_norms[:,i])-np.nanmin(Tsys_norms[:,i])) / np.nanmean(Tsys_norms[:,i])
     xdata = WVR_norms_sel; ydata = Tsys_norms_sel[:,i]
     idx_nnan = ((~np.isnan(xdata)) & (~np.isnan(ydata)))
     results = np.polyfit(xdata[idx_nnan], ydata[idx_nnan], 1, full=True)
     fit_coeff = results[0]
-    fit_err = results[1]
-    fit_err_rel = np.sqrt(fit_err / len(ydata[idx_nnan])) / np.mean(ydata[idx_nnan])
+    Tsys_norms_ext = fit_coeff[0] * WVR_norms + fit_coeff[1]
+    fit_err_mean = np.nanmean((Tsys_norms_ext-Tsys_norms[:,i])**2)    
+    fit_err_rel = np.sqrt(fit_err_mean) / np.nanmean(Tsys_norms[:,i])
     fit_results[spw]['coeff'] = fit_coeff
     fit_results[spw]['rel_err'] = fit_err_rel
+    fit_results[spw]['Tsys_range'] = Tsys_range
 
 ## add the basic information of the fit_results
 fit_results['info'] = {}
